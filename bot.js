4f65253d5cf96e7ee09d98f9b203c8a1e28f36a3
hellorkan <44034112+hellorkan@users.noreply.github.com>
2019-05-06 16:56:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,6 +31,24 @@ client.on('ready', () => {
 });
 
 
+
+client.on("message", message => {
+    if (message.content.startsWith("-bc")) {
+                 if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' ');
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+  m.send(`${argresult}\n ${m}`);
+  })
+  message.channel.send(`\`${message.guild.members.filter( m => m.presence.status !== 'all').size}\`:mailbox:  عدد المستلمين`);
+  message.delete();
+  };
+  });
+
+
+
+
+
 client.on('message', message => {
   if (message.content === "*bc" ) {
 message.channel.sendMessage("**:stopwatch:   | Sending your message...**");
